a73c449a7efd85553b6678e5633bafcb478b4aab
s049 <tofoth@naver.com>
2020-07-27 15:43:50 +0900

    각각의 메소드 및 결과값에 대한 주석 추가

--- day1/day01/day_01_mission/day_01_mission/main.swift
+++ day1/day01/day_01_mission/day_01_mission/main.swift
@@ -12,31 +12,31 @@ let keyboard: [[String]] = [["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"],
                             ["Q", "W", "E", "R", "T", "Y", "U", "I", "O", "P"],
                             ["A", "S", "D", "F", "G", "H", "J", "K", "L", ";"],
                             ["Z", "X", "C", "V", "B", "N", "M", ",", ".", "?"]]
-
 let keyboardRow: Int = keyboard.count;
 let keyboardCor: Int = keyboard[0].count;
-
-//상하가 우선 좌우가 나중
 
 var tempX: Int = 0
 var tempY: Int = 0
 var targetX: Int
 var targetY: Int
 
-
+// 입력 받는 부분
 if let words: String = readLine()! {
     
     var resultWords: String = ""
     var inputCheck: Bool = false
     
+    // input만큼 돌면서 draw 하기
     for targetWord in words {
         (targetX,targetY) = findDirect(targetWord: String(targetWord))
         
+        // 키보드를 넘어가는 입력을 받았을시
         if targetX < 0 || targetY < 0 || targetX >= keyboardRow || targetY >= keyboardCor {
             inputCheck = true
             break
         }
         
+        //움직여야할 칸수 찾기
         let (intervalX,intervalY) = findInterval(tempX: tempX, tempY: tempY, targetX: targetX, targetY: targetY, resultWords: resultWords)
         
         if intervalX != 0 {
@@ -46,6 +46,7 @@ if let words: String = readLine()! {
             resultWords += drawRightLeft(intervalY: intervalY)
         }
         
+        //이동
         tempX = targetX
         tempY = targetY
         
@@ -61,6 +62,7 @@ if let words: String = readLine()! {
     print("Input nil!")
 }
 
+// 키보드에서 target 위치 찾기 메소드
 func findDirect(targetWord: String) -> (Int, Int) {
     
     for x in 0..<keyboardRow {
@@ -71,9 +73,11 @@ func findDirect(targetWord: String) -> (Int, Int) {
         }
     }
     
+    // 잘못된 입력일시 출력
     return (-1,-1)
 }
 
+// 움직일 칸수 구하는 메소드
 func findInterval(tempX: Int, tempY: Int, targetX: Int, targetY: Int, resultWords: String) -> (Int, Int) {
     
     let intervalX = tempX - targetX
@@ -82,6 +86,7 @@ func findInterval(tempX: Int, tempY: Int, targetX: Int, targetY: Int, resultWord
     return (intervalX, intervalY)
 }
 
+// 상하 그리기
 func drawUpDown(intervalX: Int) -> String {
     var tmpResultWords : String = ""
     
@@ -98,7 +103,7 @@ func drawUpDown(intervalX: Int) -> String {
     return tmpResultWords
 }
     
-
+// 좌우 그리기
 func drawRightLeft(intervalY: Int) -> String {
     var tmpResultWords : String = ""
     
